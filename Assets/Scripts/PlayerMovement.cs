23037cc9a7fd8ebc467f957b7c9bf1f7e44e032f
louloulouloulouloulou <lourencopinto6@gmail.com>
2024-11-13 21:01:19 +0000

    aaaaaaaa
    criei jogador e chao, ele ja anda mas o salto ta esquisito

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+
+    private void Awake()
+    {
+        //Grabs references for rigidbody and animator from game object.
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Vector3 currentScale = transform.localScale;
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        //Flip player when facing left/right.
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(-Mathf.Abs(currentScale.x), currentScale.y, currentScale.z);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(Mathf.Abs(currentScale.x), currentScale.y, currentScale.z);
+
+        if (Input.GetKey(KeyCode.Space) == true && grounded == true)
+        {
+            Jump();
+        }
+            
+
+        //sets animation parameters
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+}
