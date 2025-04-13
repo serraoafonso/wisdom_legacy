6a28e317824a4e44e4046299ee25a02ab91b380a
Afonso <afonsoserrao07@gmail.com>
2025-04-13 22:39:37 +0100

    tenho de por os garinos na loja e aquele botao ta estranho

--- Assets/Scripts/HandleOpenLevel.cs
+++ Assets/Scripts/HandleOpenLevel.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HandleOpenLevel : MonoBehaviour
 {
     public GameObject LevelsScreen;
     public GameObject Level1;
+    public Button ButtonLevel1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (GameData.concluded)
+        {
+            Image imagem = ButtonLevel1.GetComponent<Image>();
+
+            Color cor = imagem.color;
+            cor.a = 0.5f;
+            imagem.color = cor;
+            ButtonLevel1.interactable = false;
+        }
     }
 
     public void onLevel1Clicked()
     {
-        LevelsScreen.SetActive(false);
-        Level1.SetActive(true);
+        if (GameData.concluded == false)
+        {
+            LevelsScreen.SetActive(false);
+            Level1.SetActive(true);
+        }
+        
     }
     // Update is called once per frame
     void Update()
